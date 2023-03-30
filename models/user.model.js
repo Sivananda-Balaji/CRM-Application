24aaf99b56aa50ffad4ba950a3382497582171a4
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-03-30 10:49:02 +0530

    add model files

---
+++ models/user.model.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  userType: {
+    type: String,
+    default: "CUSTOMER",
+  },
+  userStatus: {
+    type: String,
+    required: true,
+    default: "APPROVED",
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
