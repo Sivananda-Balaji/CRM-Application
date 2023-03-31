85d60894b088157747b4667de8b1e028397d2eaf
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-03-31 14:35:15 +0530

    add route for signIn

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,9 @@
-const { signUp } = require("../controllers/auth.controller");
+const { signUp, signIn } = require("../controllers/auth.controller");
 const express = require("express");
 
 const router = express.Router();
 
 router.route("/signup").post(signUp);
+router.route("/signIn").post(signIn);
 
 module.exports = router;
