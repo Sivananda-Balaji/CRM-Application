13516bd76aef7175583632ed814142ae2b943c01
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-04 13:17:31 +0530

    add middleware folder. This folder contains the custom middleware required in the project.

---
+++ middlewares/auth.js
@@ -0,0 +1,64 @@
+const jwt = require("jsonwebtoken");
+const { secret } = require("../configs/auth.config");
+const User = require("../models/user.model");
+
+const verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+  if (!token) {
+    return res.status(403).send({
+      message: "no token provided",
+    });
+  }
+
+  jwt.verify(token, secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: "unauthorized user",
+      });
+    }
+    req.userId = decoded.id;
+  });
+  next();
+};
+
+const isAdmin = async (req, res, next) => {
+  const user = await User.findOne({ userId: req.userId });
+  if (user && user.userType === "ADMIN") {
+    next();
+  } else {
+    return res.status(403).send({
+      message: "Admin only access the user details",
+    });
+  }
+};
+
+const checkUserType = async (req, res, next) => {
+  try {
+    const loggedInUser = await User.findOne({ userId: req.userId });
+    const userToUpdate = await User.findOne({ userId: req.params.id });
+
+    const allowedUserTypes = ["ADMIN", "ENGINEER", "CUSTOMER"];
+
+    if (!allowedUserTypes.includes(loggedInUser.userType)) {
+      return res.status(403).send({ message: "User Type is not valid!" });
+    }
+
+    if (
+      (loggedInUser && loggedInUser.userType === "ADMIN") ||
+      loggedInUser.userId === userToUpdate.userId
+    ) {
+      next();
+    } else {
+      return res.status(403).send({
+        message: "Admin or Account Owner should update the user account!",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: err,
+    });
+  }
+};
+
+module.exports = { verifyToken, isAdmin, checkUserType };
