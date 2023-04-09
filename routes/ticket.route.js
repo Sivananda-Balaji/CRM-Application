d42336470c302c8dc953d368ae5f843acb6d7bdd
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-04-09 22:04:19 +0530

    Add update and get ticket route to the app.

--- routes/ticket.route.js
+++ routes/ticket.route.js
@@ -1,10 +1,26 @@
 const express = require("express");
-const { createTicket } = require("../controllers/ticket.controller");
+const {
+  createTicket,
+  updateTicket,
+  getAllTickets,
+  getTicket,
+} = require("../controllers/ticket.controller");
 const { validateRequest } = require("../middlewares/verifyTicket");
-const { verifyToken } = require("../middlewares/auth");
+const {
+  verifyToken,
+  ticketPermission,
+  isAdmin,
+} = require("../middlewares/auth");
 
 const router = express.Router();
 
-router.route("/").post([verifyToken, validateRequest], createTicket);
+router
+  .route("/")
+  .post([verifyToken, validateRequest], createTicket)
+  .get(verifyToken, getAllTickets);
+router
+  .route("/:id")
+  .put([verifyToken, ticketPermission], updateTicket)
+  .get([verifyToken, isAdmin], getTicket);
 
 module.exports = router;
